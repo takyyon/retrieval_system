0a72f1d170e9a802524f92013a5a86a4a88c24a9
tachyon <mittal.kr@husky.neu.edu>
2019-04-19 00:20:17 -0400

    read model's top k docs

--- program.py
+++ program.py
@@ -8,13 +8,15 @@ class Program:
         self.crawler = Crawler()
         self.gram = Gram()
         self.indexer = Indexer()
+        self.baseline_runs = Baseline_Runs()
 
     def run(self):
         # self.crawler.run()
         # self.gram.run()
         # self.indexer.run()
-        self.indexer.read_positional_index()
+        # self.indexer.read_positional_index()
         # self.crawler.save_doc_length()
+        self.baseline_runs.run()
 
 program = Program()
 program.run()
--- tasks/baseline_runs.py
+++ tasks/baseline_runs.py
@@ -2,6 +2,7 @@ from extras import *
 from common import Common
 from indexer import Indexer
 import math
+import operator
 
 class Baseline_Runs:
 
@@ -14,7 +15,7 @@ class Baseline_Runs:
         self.common = Common()
         self.indexer = Indexer()
 
-    def run_bm25_for_query_and_document(query, doc, total_doc_length, doc_length, total_docs, index):
+    def run_bm25_for_query_and_document(self, query, doc, total_doc_length, doc_length, total_docs, index):
         k1 = 1.2
         b = 0.75
         k2 = 100.0
@@ -33,7 +34,7 @@ class Baseline_Runs:
             if q in index:
                 n = len(index[q])*1.0
                 if doc in index[q]:
-                    f = len(index[q][doc])*1.0
+                    f = index[q][doc]*1.0
             bm25 += (math.log(((r + 0.5)/(R - r + 0.5))/((n - r + 0.5)/(N - n - R + r + 0.5))) * (((k1 + 1)*f)/(K + f)) * (((k2 + 1)*qf)/(k2 + qf)))
         return bm25
 
@@ -48,15 +49,14 @@ class Baseline_Runs:
         print('\n' + self.utility.line_break + '\n' +\
             'Processing unigrams index and running BM25...' +\
                 'Processed data is available under ' + bm25_path)
-        
         for i in range(len(queries)):
             q = queries[i].lower().strip()
             bm25 = {}
             for d in docs:
-                bm25[d] = run_bm25_for_query_and_document(q, d,\
+                bm25[d] = self.run_bm25_for_query_and_document(q, d,\
                     total_doc_length, doc_length[d], len(docs), index)
-            bm25_sorted = sorted(bm25.items(), key=operator.itemgetter(1))
-            save_score(bm25_path + str(i), bm25_sorted, str(i), 'BM25')
+            
+            self.save_score(bm25_path + str(i), bm25, str(i), 'BM25')
 
     def get_docs_by_relevancy(self, index, docs, query):
         query_terms = query.split()
@@ -111,14 +111,15 @@ class Baseline_Runs:
             relevant_docs, non_relevant_docs = self.get_docs_by_relevancy(index, docs, q)
             prob_relevant = self.get_probability_for_docs(index, relevant_docs, q)
             non_prob_relevant = self.get_probability_for_docs(index, non_relevant_docs, q)
-            prob_relevant_docs = (relevant_docs*1.0)/(relevant_docs + non_relevant_docs)
-            prob_non_relevant_docs = (non_relevant_docs*1.0)/(relevant_docs + non_relevant_docs)
+            len_relevant_docs = len(relevant_docs)
+            len_non_relevant_docs = len(non_relevant_docs)
+            prob_relevant_docs = (len_relevant_docs*1.0)/(len_relevant_docs + len_non_relevant_docs)
+            prob_non_relevant_docs = (len_non_relevant_docs*1.0)/(len_relevant_docs + len_non_relevant_docs)
             bim = {}
             for d in docs:
-                bim[d] = run_binary_independence_model_for_query_and_document(index, q, d,\
+                bim[d] = self.run_binary_independence_model_for_query_and_document(index, q, d,\
                     prob_relevant, non_prob_relevant, prob_relevant_docs, prob_non_relevant_docs)
-            bim_sorted = sorted(bim.items(), key=operator.itemgetter(1))
-            save_score(bim_path + str(i), bim_sorted, str(i), 'Binary Independence')
+            self.save_score(bim_path + str(i), bim, str(i), 'Binary-Independence')
 
     def run_tf_idf_for_query_and_document(self, index, query, doc, doc_length, total_docs):
         query_terms = query.split()
@@ -126,7 +127,7 @@ class Baseline_Runs:
         for q in query_terms:
             tf_idf = 0.0
             if q in index and doc in index[q]:
-                tf_idf = index[q][doc]/(doc_length*1.0)
+                tf_idf = (index[q][doc])/(doc_length*1.0)
             if q in index:
                 tf_idf *= math.log((total_docs*1.0)/len(index[q]))
             total += tf_idf
@@ -141,17 +142,37 @@ class Baseline_Runs:
         index = self.indexer.read_simple_index(folder)
         print('\n' + self.utility.line_break + '\n' +\
             'Processing unigrams index and running TF-IDF Model.' +\
-                'Processed data is available under ' + bim_path)
+                'Processed data is available under ' + tf_idf_path)
         for i in range(len(queries)):
             q = queries[i].lower().strip()
             tf_idf = {}
             for d in docs:
-                tf_idf[d] = run_tf_idf_for_query_and_document(index, q, d, doc_length, len(docs))
-            tf_idf_sorted = sorted(tf_idf.items(), key=operator.itemgetter(1))
-            save_score(tf_idf_path + str(i), tf_idf, str(i), 'TF-IDF')
+                tf_idf[d] = self.run_tf_idf_for_query_and_document(index, q, d, doc_length[d], len(docs))
+            self.save_score(tf_idf_path + str(i), tf_idf, str(i), 'TF-IDF')
 
-    def save_score(filename, scores, id, score_type):
+    def save_score(self, filename, scores, id, score_type):
         data = ''
-        for score in scores:
-            data += id + ' Q0 ' + str(score[0]) + '  ::  ' + str(score[1]) + '  ' + score_type + '\n'
+        sorted_scores = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+        for score in sorted_scores:
+            data += id + '  Q0  ' + str(score[0]) + '  ' + str(score[1]) + '  ' + score_type + '\n'
         self.file_handling.save_file(data, filename)
+
+    def read_top_documents_for_score(self, folder='test-collection', query_index = 0,\
+        top = 100, score='bm25'):
+        model_file_path = 'files/' + folder + '/' + score + '/' + query_index
+        lines = self.file_handling.read_file_lines(model_file_path)
+        top_docs = []
+        i = 0
+        for l in lines:
+            if i == top:
+                break
+            data = l.split()
+            top_docs.append(data[2])
+            i += 1
+        return top_docs
+
+    def run(self, folder='test-collection'):
+        queries = self.common.get_queries(folder)
+        # self.run_bm25(queries, folder)
+        # self.run_tf_idf(queries, folder)
+        self.run_binary_independence_model(queries, folder)
